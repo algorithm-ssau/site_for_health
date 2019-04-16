18de3f5604c65a9fdf652ad4ee5be01dba84aaf6
Anastasia Khoroshilova <nastakhoroshilova@gmail.com>
2019-04-16 13:53:53 +0400

    start page

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+
+ <head>
+    <title>Start Doing Sport</title>
+
+  </head>
+
+  <body>
+      <header>
+
+        <div class="header-logo"></div>     
+
+        <div class="menu">
+          <ul>
+
+            <li>
+              <a class="menu-item"></a>
+            </li>
+
+            <li>
+              <a class="menu-item"></a>
+            </li>
+
+            <li>
+              <a class="menu-item"></a>
+            </li>
+
+          </ul>
+        </div>
+
+        <div class="share-button"></div>
+        <div class="male-button"></div>
+
+      </header>
+
+      <div class="lesson-next"></div>
+      <div class="lesson-previous"></div>
+
+      <div class="view-middle">
+
+        <div class="view-logo"></div>
+
+        <div class="view-title"></div>
+
+        <div class="view-description"></div>
+
+        <div class="start-button"></div>
+
+      </div>
+      <footer>
+
+        <div class="with-love"></div>
+        <div class="about-us"></div>
+
+      </footer>
+  </body>
+</html>
